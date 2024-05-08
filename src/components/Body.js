1b2213c49dc62a1f08b3ab5dabd9456c5383f8df
bahnisikhadhar <bahnisikhadhar@gmail.com>
2024-05-08 12:13:00 +0530

    cors issue resolved using express and body's css changes made

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,73 +11,75 @@ import Filter from './Filter';
 import SearchCity from './SearchCity';
 
 function Body() {
-  const [coordinates, setCoordinates] = useState({ lat: 19.159014, lng: 72.9985686 });
+  const [coordinates, setCoordinates] = useState({ lat: '10.5270099', lng: '76.214621' });
   // const [restoListUrl, setRestoListUrl] = useState(`${RESTO_LIST_URL}?lat=${coordinates.lat}&lng=${coordinates.lng}&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`);
   const [restoListUrl, setRestoListUrl] = useState(`http://localhost:3001/api/restaurants?lat=${coordinates.lat}&lng=${coordinates.lng}&page_type=DESKTOP_WEB_LISTING`);
+  // const [restoListUrl, setRestoListUrl] = useState(`https://food-app-be-i13l.onrender.com/api/restaurants?lat=${coordinates.lat}&lng=${coordinates.lng}&page_type=DESKTOP_WEB_LISTING`);
   const { data, isPending, error } = useFetch(restoListUrl);
   const [showShimmer, setShowShimmer] = useState(true);
   const [restaurantList, setRestaurantList] = useState([]);
   const [offset, setOffset] = useState(15);
   const [sortBy, setSortBy] = useState('RELEVANCE');
-
+// console.log(data?.data?.cards)
   useEffect(() => {
     if (data?.data?.cards) {
-      setRestaurantList(prevList => [...prevList, ...data?.data?.cards]);
-      
+      setRestaurantList(prevList => [...prevList, ...data?.data?.cards]);     
     }
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
   }, [data]);
-
+// console.log(coordinates)
 
   useEffect(() => {
     setRestaurantList([]);
     setOffset(15); 
-    //setRestoListUrl(`${RESTO_LIST_URL}?lat=${coordinates.lat}&lng=${coordinates.lng}&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`);
+    setRestoListUrl(`https://food-app-be-i13l.onrender.com/api/restaurants?lat=${coordinates.lat}&lng=${coordinates.lng}&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`);
    
   }, [coordinates]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollHeight = document.documentElement.scrollHeight;
-      const scrollTop = document.documentElement.scrollTop;
-      const clientHeight = document.documentElement.clientHeight;
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollHeight = document.documentElement.scrollHeight;
+  //     const scrollTop = document.documentElement.scrollTop;
+  //     const clientHeight = document.documentElement.clientHeight;
 
-      if (scrollTop + clientHeight >= scrollHeight) {
-        if (restaurantList) {
-          console.log("offset block")
-          setOffset(prevOffset => prevOffset + 15);
-          setRestoListUrl(
-            `${RESTO_LIST_URL}?lat=${coordinates.lat}&lng=${coordinates.lng}&offset=${offset}&sortBy=${sortBy}&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`
-          );
+  //     if (scrollTop + clientHeight >= scrollHeight) {
+  //       if (restaurantList) {
+  //         console.log("offset block")
+  //         setOffset(prevOffset => prevOffset + 15);
+  //         setRestoListUrl(
+  //           `http://localhost:3001/api/restaurants?lat=${coordinates.lat}&lng=${coordinates.lng}&offset=${offset}&sortBy=${sortBy}&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`
+  //         );
          
-        }
-      }
-    };
+  //       }
+  //     }
+  //   };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [restaurantList, coordinates.lat, coordinates.lng, offset,sortBy]);
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, [restaurantList, coordinates.lat, coordinates.lng, offset,sortBy]);
 
   useEffect(() => {
     if (data) {
       setShowShimmer(false);
+      setRestaurantList(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
-    console.log("6444",data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0].info)
+    // console.log("check",data)
+    // console.log("6444",data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   }, [data]);
-
   return (
     <>
       <Slider />
-      <SearchCity setCoordinates={setCoordinates} setRestoListUrl={setRestoListUrl} setRestaurantList={setRestaurantList} offset={offset} setOffset={setOffset}  />
-      <Filter setRestoListUrl={setRestoListUrl} setRestaurantList={setRestaurantList} coordinates={coordinates} setSortBy={setSortBy} offset={offset} setOffset={setOffset}/>
+       <SearchCity setCoordinates={setCoordinates} setRestoListUrl={setRestoListUrl} setRestaurantList={setRestaurantList} offset={offset} setOffset={setOffset}  /> 
+      {/* <Filter setRestoListUrl={setRestoListUrl} setRestaurantList={setRestaurantList} coordinates={coordinates} setSortBy={setSortBy} offset={offset} setOffset={setOffset}/> */}
       <div className={styles.cards}>
         {error && <Error error={error} />}
-        {restaurantList.map((restaurant) => (
+        {restaurantList.map((restaurant, index) => (
           <Link
-            to={'/restaurant/' + restaurant?.data?.data?.id}
-            key={restaurant?.data?.id}
+            to={'/restaurant/' + restaurant?.info?.id}
+            key={restaurant?.info?.id}
             className={styles.card}
           >
-            <RestaurantCard {...restaurant?.data?.data} />
+            <RestaurantCard restaurant={restaurant?.info} />
           </Link>
         ))}
         {showShimmer && <Shimmer />}
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,32 +2,27 @@ import React from 'react'
 import { IMG_BASE_URL } from '../utils/constants'
 import styles from "./RestaurantCard.module.css"
 
-function RestaurantCard({
-  cloudinaryImageId,
-  name,
-  cuisines,
-  area,
-  lastMileTravelString,
-  costForTwoString,
-  avgRating,
-  minDeliveryTime
-}) {
-
-  let random = (Math.random()* (5 - 3)+3 ).toFixed(1);
+function RestaurantCard(restaurant) {
+   let random = (Math.random()* (5 - 3)+3 ).toFixed(1);
+   console.log(restaurant)
   return (
    
     <div>
-      <img className={styles.image} src={IMG_BASE_URL + cloudinaryImageId} alt="" />
+      <img className={styles.image} src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + restaurant?.restaurant?.cloudinaryImageId} alt="" />
       <div className={styles.details}>
-        <h2 className={styles.title}>{name}</h2>
-        <h5 className={styles.subtitle}>{cuisines.join(", ")}</h5>
-        <h6 className={styles.area}>{area}</h6>
+        <h2 className={styles.title}>{restaurant?.restaurant?.name}</h2>
+        <h5 className={styles.subtitle}>{restaurant?.restaurant?.cuisines.join(", ")}</h5>
+        <h6 className={styles.area}>{restaurant?.restaurant?.areaName}</h6>
         <div className={styles.info}>
-          <h4 className={styles.infoItem}>{costForTwoString}</h4>
-          <h5 className={(avgRating>4 || random>4)? styles.infoItemTop : styles.infoItemPoor}><i class="fa-solid fa-star" style={{color:"white",fontSize:".7rem"}}></i> {(avgRating>4)?avgRating:random}</h5>
-          <h5 className={styles.infoItem}>{minDeliveryTime} mins</h5>
+          <h4 className={styles.infoItem}>{restaurant?.restaurant?.costForTwo}</h4>
+          <h5 className={(restaurant?.restaurant?.avgRating>4 || random>4)? styles.infoItemTop : styles.infoItemPoor}><i className="fa-solid fa-star" style={{color:"white",fontSize:".7rem"}}></i> {(restaurant?.restaurant?.avgRating>4)?restaurant?.restaurant?.avgRating:random}</h5>
+          <h5 className={styles.infoItem}>{restaurant?.restaurant?.isOpen ? "Open": "Closed"} </h5>
+         
           <h5>{}</h5>
         </div>
+        {/* <div className={styles.info}>
+        <h5 className={styles.infoItem}>{restaurant?.restaurant?.sla?.slaString}</h5>
+          </div> */}
       </div>
     </div>
    
