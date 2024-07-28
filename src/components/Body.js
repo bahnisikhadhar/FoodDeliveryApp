66a4bbcafacdb64a183b6aea1072bb682c3b4fa8
bahnisikhadhar <bahnisikhadhar@gmail.com>
2024-07-28 22:13:15 +0530

    made modification in seo

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,8 +13,8 @@ import SearchCity from './SearchCity';
 function Body() {
   const [coordinates, setCoordinates] = useState({ lat: '22.5726459', lng: '88.3638953' });
   // const [restoListUrl, setRestoListUrl] = useState(`${RESTO_LIST_URL}?lat=${coordinates.lat}&lng=${coordinates.lng}&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`);
-  // const [restoListUrl, setRestoListUrl] = useState(`http://localhost:3001/api/restaurants?lat=${coordinates.lat}&lng=${coordinates.lng}&page_type=DESKTOP_WEB_LISTING`);
-  const [restoListUrl, setRestoListUrl] = useState(`https://fooddeliveryapp-server-nftc.onrender.com/api/restaurants?lat=${coordinates.lat}&lng=${coordinates.lng}&page_type=DESKTOP_WEB_LISTING`);
+  const [restoListUrl, setRestoListUrl] = useState(`http://localhost:3001/api/restaurants?lat=${coordinates.lat}&lng=${coordinates.lng}&page_type=DESKTOP_WEB_LISTING`);
+  // const [restoListUrl, setRestoListUrl] = useState(`https://fooddeliveryapp-server-nftc.onrender.com/api/restaurants?lat=${coordinates.lat}&lng=${coordinates.lng}&page_type=DESKTOP_WEB_LISTING`);
   
   const { data, isPending, error } = useFetch(restoListUrl);
   const [showShimmer, setShowShimmer] = useState(true);
